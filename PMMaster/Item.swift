c8908d6065359c834a54e8f859ad5db1640b2aef
Fantom <ivanneporadnyj@gmail.com>
2026-03-10 19:25:59 +0200

    Initial Commit

---
+++ PMMaster/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PMMaster
+//
+//  Created by Иван Непорадный on 10.03.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
